aac5624ca81ad14869188ccbfa89104e69b48e3a
Sofiya <925038623sss@gmail.com>
2024-06-05 12:38:55 +0300

    commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
     public int calculate(int price) {
-        int oneMilesPrice =20;
-        return price /oneMilesPrice;
+        int oneMilesPrice = 20;
+        return price / oneMilesPrice;
     }
 }
